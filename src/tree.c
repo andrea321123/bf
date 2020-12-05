46b2b7f017f81e448a84944c3931b2d849d75de6
andrea321123 <gal.andrea.van@gmail.com>
2020-12-05 19:31:42 +0100

    Add tree implementation

---
+++ src/tree.c
@@ -0,0 +1,38 @@
+// tree.c
+// Version 1.0
+
+#include <stddef.h>
+#include <stdlib.h>
+
+#include "tree.h"
+
+Tree *initTree() {
+    Tree *head = malloc(sizeof(Tree));
+
+    head->nChildren = 0;
+    head->children = NULL;
+}
+
+void freeTree(Tree *node) {
+    // recursive action: we free all the children
+    for (uint64_t i = 0; i < node->nChildren; i++) {
+        freeTree(node->children[i]);
+    }
+
+    free(node);
+}
+
+Tree *addChild(Tree *node, Token value) {
+    node->nChildren++;
+    
+    // create the new node
+    Tree *newNode;
+    newNode = initTree();
+    newNode->value = value;
+
+    // add newNode to the tree children
+    node->children = realloc(node->children, node->nChildren);
+    node->children[node->nChildren -1] = newNode;
+
+    return newNode;
+}
---
+++ src/tree.h
@@ -0,0 +1,32 @@
+// tree.h
+// Version 1.0
+
+#ifndef TREE_H
+#define TREE_H
+
+#include <stdint.h>
+
+#include "token.h"
+
+typedef struct TokenTree Tree;
+
+// node of the Abstract Syntax tree
+struct TokenTree {
+    Token value;
+
+    struct TokenTree **children;
+    uint64_t nChildren;
+};
+
+
+
+// initialize a Tree pointer 
+Tree *initTree();
+
+// free a tree starting from the parameter pointer
+void freeTree(Tree *node);
+
+// add a new node to the parameter tree
+Tree *addChild(Tree *node, Token value);
+
+#endif
