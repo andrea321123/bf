8fec4016ec042a7f25e600dfd42acede1109fe86
andrea321123 <gal.andrea.van@gmail.com>
2020-12-06 12:42:14 +0100

    Create pipeline

--- src/main.c
+++ src/main.c
@@ -1,6 +1,73 @@
-#include <stdio.h>
+// main.c
+// Version 1.0
 
-int main() {
-    printf("Hello, World!\n");
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "lexer.h"
+#include "parser.h"
+#include "interpreter.h"
+
+// private function declarations
+char *readSourceCode(char *path);
+
+int main(int argc, char *argv[]) {
+    // argv[1] must be path to source code
+    if (argc <2) {
+        printf("Specify source code path\n");
+        exit(4);
+    }
+
+    char *sourceCode = readSourceCode(argv[1]);
+
+    // pipeline:
+    // - lexing
+    // - parsing
+    // - evaluation
+    List *list = doLexing(sourceCode);
+    
+    Tree *tree = doParsing(list);
+
+    Interpreter *interpreter = initInterpreter();
+    run(interpreter, tree);
+
+    // free all allocated memory
+    free(sourceCode);
+    freeList(list);
+    freeTree(tree);
+    freeInterpreter(interpreter);
+
+    printf("OPREMENERE");
+    int sas = 0;
+    scanf("%d", &sas);
     return 0;
+}
+
+char *readSourceCode(char *path) {
+    FILE *file = fopen(path, "r");
+
+    if (file == NULL) {
+        printf("Could not open file\n");
+        exit (5);
+    }
+
+    char *sourceCode = malloc(100);
+    char character;
+    uint64_t allocatedSize = 100;
+    uint64_t offset = 0;
+
+    // read file char by char
+    while ((character = fgetc(file)) != EOF) {
+        // check if allocated space is enough
+        if (offset >= allocatedSize) {
+            allocatedSize *=2;
+            sourceCode = realloc(sourceCode, allocatedSize);
+            printf("%lu\n", allocatedSize);
+        }
+
+        sourceCode[offset++] = character;
+    }
+
+    sourceCode[offset] = '\0';         // add final character
+    return sourceCode;
 }
